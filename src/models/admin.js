fa7c80b07bdecadedc0596298e5e48a4167dfff8
Youssef-Ashraf2099 <ya1684657@gmail.com>
2025-03-05 20:35:30 +0200

    admin added

---
+++ src/models/admin.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const adminschema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      lowercase: true,
+      required: true,
+      validator(value) {
+        if (!validator.isEmail(value)) {
+          throw new error("invalid email");
+        }
+      },
+    },
+    profilepic: {
+      type: String, //url or path to profile picture
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 7,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Admin = mongoose.model("Admin", adminschema);
+
+module.exports = Admin;
