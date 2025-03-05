25d137c630d2cac3d3ddb2b1a147c738cfa2201b
Youssef-Ashraf2099 <ya1684657@gmail.com>
2025-03-05 20:51:28 +0200

    resolve conflict 3beet awy bsabeb zakzouk basha

--- src/models/booking.js
+++ src/models/booking.js
@@ -1,33 +1,36 @@
 const mongoose = require("mongoose");
 
-const BookingSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
+const BookingSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    event: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+      required: true,
+    },
+    numberOfTickets: {
+      type: Number,
+      required: true,
+      //min: 1,// This line is to be discussed on how to handle the attributes
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "confirmed", "canceled"],
+      default: "pending",
+    },
   },
-  event: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Event',
-    required: true,
-  },
-  numberOfTickets: {
-    type: Number,
-    required: true,
-    //min: 1,// This line is to be discussed on how to handle the attributes
-  },
-  totalPrice: {
-    type: Number,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ["pending", "confirmed", "canceled"],
-    default: "pending",
-  },
-}, {
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Booking = mongoose.model("Booking", BookingSchema);
 
