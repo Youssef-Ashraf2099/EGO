2b41ddcd347008648cf8ed6e6d233fdeb1426bea
Youssef-Ashraf2099 <ya1684657@gmail.com>
2025-03-05 21:23:24 +0200

    checked on the database schema

--- src/models/event.js
+++ src/models/event.js
@@ -1,45 +1,52 @@
 const mongoose = require("mongoose");
 
-const EventSchema = mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const EventSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    ticketPrice: {
+      type: Number,
+      required: true,
+    },
+    ticketAvailable: {
+      type: Number,
+      required: true,
+    },
+    ticketSold: {
+      type: Number,
+      default: 0,
+    },
+    organizer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    required: true,
-  },
-  location: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-  },
-  ticketPrice: {
-    type: Number,
-    required: true,
-  },
-  ticketAvailable: {
-    type: Number,
-    required: true,
-  },
-  organizer: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-}, {
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Event = mongoose.model("Event", EventSchema);
 
--- src/models/user.js
+++ src/models/user.js
@@ -2,48 +2,69 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is invalid");
-      }
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is invalid");
+        }
+      },
+    },
+    profilePicture: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8, // This line is to be discussed on how to handle the attributes
+      trim: true,
+    },
+    role: {
+      type: String,
+      enum: ["Standard User", "Organizer", "System Admin"],
+      default: "Standard User",
     },
   },
-  profilePicture: {
-    type: String,
-  },
-  password: {
-    type: String,
-    required: true,
-    //minlength: 7,// This line is to be discussed on how to handle the attributes
-    trim: true,
-  },
-  role: {
-    type: String,
-    enum: ["Standard User", "Organizer", "System Admin"],
-    default: "Standard User",
-  },
-}, {
-  timestamps: true,
+  {
+    timestamps: true,
+  }
+);
+
+UserSchema.virtual("events", {
+  //this is a virtual field where we can get the events that the user(orgnizer) has organized
+  ref: "Event",
+  localField: "_id",
+  foreignField: "organizer",
 });
 
-UserSchema.virtual('events', {
-  ref: 'Event',
-  localField: '_id',
-  foreignField: 'organizer',
-});
+/*
+adding the concept of hasing the password before saving the user as a draft
 
+// Hash the password before saving the user
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+*/
 const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
