cea9f72c1e1d8d404e8964ac3b80173e87469ad4
Youssef-Ashraf2099 <ya1684657@gmail.com>
2025-04-09 05:04:30 +0200

    added basic Archectiyture files to main

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const express = require("express");
 const mongoose = require("./src/database/mongoose");
 const app = express();
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const userRouter = require("./src/routes/user");
+const eventRouter = require("./src/routes/events");
+const bookingRouter = require("./src/routes/booking");
+const authernticationMiddleware = require("./src/middleware/authenticationMiddleware");
+
+require("dotenv").config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+//app.use("/api/v1", ay routerhena);@hamza00234
+//app.use("/api/v1/user", userRouter); //public route no authentication required
+app.use(authenticationMiddleware); //any route after this line will be protected by the authentication middleware but otherwise it will be public
+//app.use("/api/v1/user", userRouter); is protected route requires authentication
 
 const PORT = process.env.PORT;
 
@@ -8,4 +35,4 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-//mongoose.listen(process.env.DATABASE_URL);
+//mongoose.listen(process.env.DATABASE_URL); //connect to the database
