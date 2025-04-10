49dd33f1a72a46c483db8db40629a4984a543831
Youssef-Ashraf2099 <ya1684657@gmail.com>
2025-04-10 21:36:31 +0200

    Fix booking route endpoint and add async handler for fetching bookings

--- src/router/booking.js
+++ src/router/booking.js
@@ -5,3 +5,12 @@ const booking = require("../model/Booking");
 router.get("/test", (req, res) => {
   res.send("testing booking route");
 });
+
+router.get("bookings", async (req, res) => {
+  try {
+    const bookings = await booking.find().populate("event").populate("user");
+    res.status(200).send(bookings);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
