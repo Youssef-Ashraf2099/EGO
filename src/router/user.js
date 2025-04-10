dbd327e371f8159f91dc6e4f138b1cb80dfd6ee3
Youssef-Ashraf2099 <ya1684657@gmail.com>
2025-04-10 21:04:32 +0200

    booking,events,event analytics added

--- src/router/user.js
+++ src/router/user.js
@@ -43,19 +43,9 @@ router.put("/forgetPassword", async (req, res) => {
   }
 });
 
-//create user
-router.post("/user", async (req, res) => {
-  const user = new User(req.body);
-  try {
-    await user.save();
-    res.status(201).send(user);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
 //get all users
 router.get("/users", async (req, res) => {
+  //@ziyadzakzouk add authentication middleware as admin here lama t5les el middleware bta3tek
   try {
     const users = await User.find();
     res.status(200).send(users);
@@ -63,9 +53,35 @@ router.get("/users", async (req, res) => {
     res.status(500).send(e);
   }
 });
-
+//get user profile and put user profile
+router.get("/users/profile", async (req, res) => {
+  //user should be authenticated to access this route @ziyadzakzouk
+  try {
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      return res.status(404).send("not found");
+    }
+    res.status(200).send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+router.put("/users/profile", async (req, res) => {
+  //user should be authenticated to access this route @ziyadzakzouk
+  try {
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      return res.status(404).send("not found");
+    }
+    Object.assign(user, req.body);
+    await user.save();
+    res.status(200).send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 //get user by id
-router.get("/user/:id", async (req, res) => {
+router.get("/users/:id", async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     res.status(200).send(user);
@@ -75,7 +91,8 @@ router.get("/user/:id", async (req, res) => {
 });
 
 //update
-router.patch("/user/:id", async (req, res) => {
+router.patch("/users/:id", async (req, res) => {
+  // put wla patch ?
   try {
     const update = req.body;
     const user = await User.findById(req.params.id);
@@ -90,7 +107,7 @@ router.patch("/user/:id", async (req, res) => {
   }
 });
 //delete
-router.delete("/user/:id", async (req, res) => {
+router.delete("/users/:id", async (req, res) => {
   try {
     const user = await User.findByIdAndDelete(req.params.id);
 
@@ -99,6 +116,51 @@ router.delete("/user/:id", async (req, res) => {
     }
 
     res.status(204).json({ message: "deleted" });
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+router.get("/users/bookings", async (req, res) => {
+  //Admin access
+  try {
+    const user = await User.findById(req.user._id).populate("bookings");
+    if (!user) {
+      return res.status(404).send("not found");
+    }
+    res.status(200).send(user.bookings);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+router.get("/users/events", async (req, res) => {
+  //Admin access
+  try {
+    const user = await User.findById(req.user._id).populate("events");
+    if (!user) {
+      return res.status(404).send("not found");
+    }
+    res.status(200).send(user.events);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+router.get("/users/events/analtyics", async (req, res) => {
+  //Admin access
+  try {
+    const user = await User.findById(req.user._id).populate("events");
+    if (!user) {
+      return res.status(404).send("not found");
+    }
+    const events = user.events;
+    const analytics = events.map((event) => {
+      return {
+        eventName: event.name,
+        totalBookings: event.bookings.length,
+        totalAttendees: event.attendees.length,
+      };
+    });
+    res.status(200).send(analytics);
   } catch (e) {
     res.status(500).send(e);
   }
