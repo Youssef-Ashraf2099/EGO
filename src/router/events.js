1333109a069796d72b57e08cabc4224ce347e0fd
Youssef-Ashraf2099 <ya1684657@gmail.com>
2025-04-10 22:00:20 +0200

    update,delete,create event

--- src/router/events.js
+++ src/router/events.js
@@ -29,3 +29,43 @@ router.get("/events/:id", async (req, res) => {
 });
 
 //protected routes by admin and organizer
+//except create event by orgnizer bas
+router.post("/events", async (req, res) => {
+  //user should be authenticated as orgnizer to access this route @ziyadzakzouk
+  try {
+    const event = new Event(req.body);
+    await event.save();
+    res.status(201).send(event);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+//update event by id
+router.patch("/events/:id", async (req, res) => {
+  try {
+    const update = req.body;
+    const event = await Event.findById(req.params.id);
+    if (!event) {
+      return res.status(404).send("not found");
+    }
+    Object.assign(event, update);
+    await event.save();
+    res.status(200).send(event);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+//delete event by id
+router.delete("/events/:id", async (req, res) => {
+  try {
+    const event = await Event.findByIdAndDelete(req.params.id);
+    if (!event) {
+      return res.status(404).send("not found");
+    }
+    res.status(200).send(event);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
