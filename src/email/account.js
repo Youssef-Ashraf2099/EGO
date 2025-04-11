ad2cbf512e74c5ae21d19541ee0a1383172ca44e
hamza00234 <hamzaa098poi@gmail.com>
2025-04-11 14:28:22 +0200

    userController and email forgotpassword finished

--- src/email/account.js
+++ src/email/account.js
@@ -2,7 +2,12 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 const nodemailer=require("nodemailer")
+//store otp to be later compared
+const otpStore = new Map();
 
+function generateOtp() {
+    return Math.floor(1000 + Math.random() * 9000).toString();
+}
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -12,22 +17,44 @@ const transporter = nodemailer.createTransport({
     }
   });
 
-  const forgotpassword=(email, name)=>{
-   
-
+  const sendOtpEmail=(email,otp)=>{
     const mailOptions = {
         from: process.env.EMAIL,
         to: email,
         subject: 'forgot password',
-        text:`hi`
+        text:`otp code is: ${otp}`
     }
     transporter.sendMail(mailOptions, function(error, info) {
         if (error) {
             console.log(error);
         } else {
-            console.log('Email is sent ' + info.response);
+            console.log('Email is sent ' + info.response)
         }
-    });
+    })
+
+  }
+
+  const forgotpassword=(email)=>{
+    const otp=generateOtp()
+    otpStore.set(email,otp)
+    sendOtpEmail(email,otp)
+
+    //expiration time for otp=5 min
+    setTimeout(() => otpStore.delete(email), 5 * 60 * 1000);
 }
 
-forgotpassword("hamzaa098poi@gmail.com","hamza")
+//verify entered otp is the same as send otp
+const verify=(email,otp)=>{
+    const storedOtp=otpStore.get(email)
+    if(storedOtp==otp){
+        otpStore.delete(email)
+        return true;
+    }
+    return false
+}
+
+
+module.exports={
+    forgotpassword,
+    verify
+}
