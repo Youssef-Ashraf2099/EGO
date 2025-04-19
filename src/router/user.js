fd0995b098c88e3eafef27265112e13287ed0dc6
Youssef-Ashraf2099 <ya1684657@gmail.com>
2025-04-19 21:14:44 +0200

    public routes added

--- src/router/user.js
+++ src/router/user.js
@@ -1,72 +1,43 @@
-const express=require('express')
-const router=express.Router()
-const User=require('../models/User')
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+const eventController = require("../controllers/eventController");
+const bookingController = require("../controllers/bookingController");
+const authenticationMiddleware = require("../middleware/authenticationMiddleware");
+const authorizationMiddleware = require("../middleware/authorizationMiddleware");
 
-router.get('/test',(req,res)=>{
-    res.send('testing')
-})
+// Public routes
+// POST /api/v1/register - Register a new user
+router.post("/register", async (req, res) => {
+  try {
+    await userController.register(req, res);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-//create user
-router.post('/user',async (req,res)=>{
-    const user=new User(req.body)
-    try{
-        await user.save()
-        res.status(201).send(user)
-    }catch(e){
-        res.status(400).send(e)
-    }
+// POST /api/v1/login - Authenticate user and return token
+router.post("/login", async (req, res) => {
+  try {
+    await userController.login(req, res);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-})
+router.post("/sendOtp", async (req, res) => {
+  try {
+    await userController.passwordResetOtp(req, res);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-//get all users
-router.get('/users',async(req,res)=>{
-    try{
-        const users=await User.find()
-        res.status(200).send(users)
-    }catch(e){
-        res.status(500).send(e)
-    }
-})
-
-//get user by id
-router.get('/user/:id',async (req,res)=>{
-    try{
-        const user=await User.findById(req.params.id)
-        res.status(200).send(user)
-
-    }catch(e){
-        res.status(400).send(e)
-    }
-})
-
-//update
-router.patch('/user/:id',async(req,res)=>{
-    try{
-        const update=req.body
-        const user=await User.findById(req.params.id)
-        if(!user){
-            return res.status(404).send("not found")
-        }
-        Object.assign(user, update);
-        await user.save();
-        res.status(200).send(user)
-    }catch(e){
-        res.status(500).send(e)
-    }
-})
-//delete
-router.delete('/user/:id',async(req,res)=>{
-    try{
-    const user = await User.findByIdAndDelete(req.params.id);
-    
-    if (!user) {
-                return res.status(404).json({ message: 'User not found' });
-            }
-    
-    res.status(204).json({message:"deleted"})
-    }catch(e){
-        res.status(500).send(e)
-    }
-})
-
-module.exports=router
+// PUT /api/v1/forgetPassword - Update user password
+router.put("/forgetPassword", async (req, res) => {
+  try {
+    await userController.resetpassword(req, res);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
