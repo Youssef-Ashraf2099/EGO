4482c3b180c5a6ffe96f8829fd528c7997fa887d
Youssef Ashraf <155368101+Youssef-Ashraf2099@users.noreply.github.com>
2025-05-18 19:32:26 +0300

    feat: add Footer component and integrate it into App

--- src/View/src/App.jsx
+++ src/View/src/App.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import './App.css';
-import axios from 'axios';  // Make sure axios is imported
-import Login from './login'; // make sure this path is correct
-import Register from './register';
-import SendOTP from './sendOtp'
-import ForgotPassword from './forgotPassword';
+import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import "./App.css";
+import axios from "axios"; // Make sure axios is imported
+import Login from "./login"; // make sure this path is correct
+import Register from "./register";
+import SendOTP from "./sendOtp";
+import ForgotPassword from "./forgotPassword";
+import Footer from "./footer";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/api/v1/login" element={<Login />} />
-      <Route path="/api/v1/register" element={<Register />} />
-      <Route path="/api/v1/sendOtp" element={<SendOTP />}/>
-      <Route path="/api/v1/forgetPassword" element={<ForgotPassword />}/>
-    </Routes>
+    <>
+      <Footer />
+      <Routes>
+        <Route path="/api/v1/login" element={<Login />} />
+        <Route path="/api/v1/register" element={<Register />} />
+        <Route path="/api/v1/sendOtp" element={<SendOTP />} />
+        <Route path="/api/v1/forgetPassword" element={<ForgotPassword />} />
+      </Routes>
+    </>
   );
 }
 
-export default App
+export default App;
