b50f28467e039ff5e22d0da46cb53e60ee5cbb84
Youssef Ashraf <155368101+Youssef-Ashraf2099@users.noreply.github.com>
2025-05-19 19:12:28 +0300

    credits added

--- src/View/src/AboutUs.jsx
+++ src/View/src/AboutUs.jsx
@@ -13,7 +13,15 @@ function AboutUs() {
       <p>
         EGO was an ambitious project for the developer, aiming to make life
         easier by simplifying the event booking process and bringing people
-        together for memorable experiences.
+        together for memorable experiences. specail thanks to the developer's
+        friends and family for their support and encouragement throughout the
+        development process. The developer's passion for technology and
+        commitment to creating innovative solutions drove the project forward,
+        and the result is a user-friendly platform that makes it easy to
+        discover and book events. EGO is designed to be a one-stop solution for
+        all your event needs, providing a seamless experience from start to
+        finish. CREDITS: 1-Youssef Ashraf 2-Mostafa Ahmed 3-Hamza Ahmed 4-Ziad
+        Amr Zakzouk 5-Abdullah Mahmoud
       </p>
     </div>
   );
