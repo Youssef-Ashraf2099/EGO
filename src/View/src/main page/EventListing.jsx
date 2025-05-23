4f99f2872f89e75bf3e0e53acc325253671a0b23
Youssef Ashraf <155368101+Youssef-Ashraf2099@users.noreply.github.com>
2025-05-23 19:52:35 +0300

    API hiiden

--- src/View/src/main page/EventListing.jsx
+++ src/View/src/main page/EventListing.jsx
@@ -1,13 +1,13 @@
-import "./styles/EventListing.css"
-import EventCard from "./EventCard"
-import { useState, useEffect } from "react"
-import axios from "axios"
-import { PacmanLoader } from "react-spinners"
+import "./styles/EventListing.css";
+import EventCard from "./EventCard";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { PacmanLoader } from "react-spinners";
+const Port = import.meta.env.VITE_PORT || 3040;
 
+axios.defaults.withCredentials = true;
 
-axios.defaults.withCredentials = true
-
-const EventsListing = ({isHome}) => {
+const EventsListing = ({ isHome }) => {
   // const events = [
   //   {
   //     id: 1,
@@ -52,31 +52,31 @@ const EventsListing = ({isHome}) => {
   //     image: "https://ik.imagekit.io/wuxgiazko/Rectangle%2012.svg?updatedAt=1747772816924",
   //   },
   // ]
-const [events, setEvents] = useState([])
-const [isLoading, setIsLoading] = useState(true)
-
+  const [events, setEvents] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchEvents = async () => {
       try {
-        const response = await axios.get(`http://localhost:3000/api/v1/events/`)
-        console.log(response.data) 
-        setEvents(response.data)
+        const response = await axios.get(
+          `http://localhost:{VITE_PORT}/api/v1/events/`
+        );
+        console.log(response.data);
+        setEvents(response.data);
       } catch (error) {
-        console.error("Error fetching events:", error)
+        console.error("Error fetching events:", error);
+      } finally {
+        setIsLoading(false);
       }
-      finally {
-        setIsLoading(false)
-      }
-    }
+    };
 
-    fetchEvents()
-  }, [])    
- 
-if (isLoading) {
+    fetchEvents();
+  }, []);
+
+  if (isLoading) {
     return (
       <div className="loader-container">
-        <PacmanLoader color="#EFBA1A" size={50}/>
+        <PacmanLoader color="#EFBA1A" size={50} />
       </div>
     );
   }
@@ -85,25 +85,35 @@ if (isLoading) {
     <section className="events-section">
       <div className="container">
         <div className="events-header">
-          <h2 className="section-title">{isHome? "Featured Events":"All Events"}</h2>
+          <h2 className="section-title">
+            {isHome ? "Featured Events" : "All Events"}
+          </h2>
         </div>
 
-        <div className="events-grid" >
+        <div className="events-grid">
           {isHome
-    ? events.slice(0, 6).map((event) => (
-        <EventCard key={event._id} event={event} />
-      ))
-    : events.map((event) => (
-        <EventCard key={event._id} event={event} />
-      ))}
+            ? events
+                .slice(0, 6)
+                .map((event) => <EventCard key={event._id} event={event} />)
+            : events.map((event) => (
+                <EventCard key={event._id} event={event} />
+              ))}
         </div>
 
         <div className="load-more">
-          {isHome?<a href="/events"><button className="btn btn-outline load-more-button">Show All</button></a>:""}
+          {isHome ? (
+            <a href="/events">
+              <button className="btn btn-outline load-more-button">
+                Show All
+              </button>
+            </a>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default EventsListing
+export default EventsListing;
