30e90644346f2bdb4feb537497b9af74894586bd
Youssef Ashraf <155368101+Youssef-Ashraf2099@users.noreply.github.com>
2025-05-26 20:43:17 +0300

    add BookingEvent.css for styling loading, error, and no data messages

--- src/View/src/BookingEvent.jsx
+++ src/View/src/BookingEvent.jsx
@@ -3,7 +3,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import EventBookingForm from "./EventBookingForm";
 import BookingDetails from "./BookingDetails";
-
+import "./BookingEvent.css";
 function BookingEvent() {
   const { id } = useParams();
   const [data, setData] = useState(null);
