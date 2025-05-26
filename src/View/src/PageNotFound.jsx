38e20613153c5237d62953fef5449e6d3d3a4f18
Youssef Ashraf <155368101+Youssef-Ashraf2099@users.noreply.github.com>
2025-05-27 01:07:24 +0300

    ABCDEFGH I AM DYING HERE

--- src/View/src/PageNotFound.jsx
+++ src/View/src/PageNotFound.jsx
@@ -1,24 +1,32 @@
-import "./pageNotFound.css"
+import "./PageNotFound.css";
 
 const pageNotFound = () => {
   return (
     <>
       <div className="hello">
-    <div className="container2">
-        <div className="error-image">
-          
-            <img src="https://ik.imagekit.io/wuxgiazko/hero-img-01.png?updatedAt=1747942369179" alt="404 Error" id="error-placeholder" />
-        </div>
-        
-        <div className="error-text">
+        <div className="container2">
+          <div className="error-image">
+            <img
+              src="https://ik.imagekit.io/wuxgiazko/hero-img-01.png?updatedAt=1747942369179"
+              alt="404 Error"
+              id="error-placeholder"
+            />
+          </div>
+
+          <div className="error-text">
             <h1>Ooops!</h1>
             <h2>Looks like you're in the wrong place.</h2>
-            <p>We can't find the page you're looking for... <a href="/" className="home-link">→ Take me home</a></p>
+            <p>
+              We can't find the page you're looking for...{" "}
+              <a href="/" className="home-link">
+                → Take me home
+              </a>
+            </p>
+          </div>
         </div>
-    </div>
-</div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default pageNotFound
+export default pageNotFound;
